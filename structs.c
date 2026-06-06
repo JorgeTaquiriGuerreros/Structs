57143c02f5568154c98f21655001372c7598a042
JorgeTaquiriGuerreros <jtaquirig@unsa.edu.pe>
2026-06-06 00:10:10 -0500

    avance del lab 2: se agregaron dos ifs mas que verifican para comprobar las comillas dobles o simples en la entrada como tambien los # para las directivas, asegura que solo pase lo que se pide palabras con subrayado

--- structs.c
+++ structs.c
@@ -31,30 +31,46 @@ int getword(char* word, int lim) { //lim = nro maximo chars que puedo tener en e
 
   if(c != EOF) *w++ = c;
 
-  if(!(isalpha(c) || c != '\"')){ //caracter alfabetico o comilla
+  if(c == '\"' || c == '\'') {
+    int quote = c; //se guarda que tipo de comilla se ingreso
+    w--;
+
+    while((c = getch()) != quote && c != EOF) {
+      if(c == '\\'){
+        *w++ = c;
+        c = getch();
+      }
+      if(lim > 1){//para no sobrepasar limite del buffer
+        *w++ = c;
+        lim--;
+      }
+    }
+    *w = '\0';
+    return word[0] ? word[0] : quote;
+  }
+
+  if(c == '#'){//para tratar directivas como include define ifndef o endif :D
+    w--;
+    while ((c = getch()) != '\n' && c != EOF){
+    //consumir toda la palabra despues del #
+    }
+    word[0] = '\0';
+    return getword(word,lim);//llamamos otra vez para seguir con la otra palabra
+  }
+
+  if(!isalpha(c) && c != '_'){ //si no es char alfabetico ni guion bajo
     *w = 0; //string acaba en 0 o '\0'
     return c;
   }
-  
-  if(c == '\"'){
-    w--;
-    while((c = getch()) != '\"'){
-      if(c == '\\'){
-        c = getch();
-      }
-      *w++ = c;
-    }
-    *w = 0;
-    return word[0];
-  }
+
   for(; --lim > 0; w++){
     if(!isalnum_(*w = getch())){ //verifica si es caracter alfab. o subrayado
-      ungetch(*w);//getch toma carac. entrada y ungetch devuelve caracter
-      break;//sino es letra o numero temina for
+      ungetch(*w);
+      break;//sino es letra o numero temina
     }
   }
 
   *w = 0;
-  return word[0];//retorna primer carac. leido
+  return word[0];
 
 }
